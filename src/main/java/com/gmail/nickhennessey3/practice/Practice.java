23d985fc7e360c376fc06583f0ebc70d56b7215a
Varitek3rulz3 <nickhennessey3@gmail.com>
2018-01-30 15:30:57 -0500

    Init
    Init

---
+++ src/main/java/com/gmail/nickhennessey3/practice/Practice.java
@@ -0,0 +1,54 @@
+package com.gmail.nickhennessey3.practice;
+
+
+import java.util.logging.Logger;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
+import com.gmail.nickhennessey3.practice.commands.PracticePluginExecutor;
+import com.google.common.collect.Lists;
+import com.google.inject.Inject;
+
+
+@Plugin(id = "practice", name = "Practice", version = "1.0")
+
+public class Practice {
+	
+	private static final String String = null;
+	@Inject
+	
+	private Logger logger;
+	
+	public Logger getLogger() 
+	{
+	
+		return logger;
+	
+	
+	}
+	
+	@Listener
+	public void onInitialize(GameInitializationEvent event)
+	{
+		
+		getLogger().info(String);
+	    
+		this.getLogger().info("Loading...");
+		
+		CommandSpec practiceCommandSpec = CommandSpec.builder()
+				.description(Text.of("Output based on Source"))
+				.executor(new PracticePluginExecutor())
+				.build();
+		
+		Sponge.getCommandManager().register(this, practiceCommandSpec, Lists.newArrayList("practice"));
+		
+		this.getLogger().info("Loaded!");
+		
+	}
+
+
+  }
+
